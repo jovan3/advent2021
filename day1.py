859a48e2a3ac5e4e4e5e6960aed0bf45c3bac300
Yovan Naumovski <jovan3.14@gmail.com>
2021-12-13 22:19:34 +0200

    day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+def parse_input():
+    with open('inputs/day1') as file:
+        lines = file.readlines()
+        return [int(line) for line in lines]
+
+parsed_input = parse_input()
+    
+def number_larger_than_previous(input):
+    count = 0
+    prev = input[0]
+    for i in range(1, len(input)):
+        if input[i] > prev:
+            count += 1
+        prev = input[i]
+
+    return count
+
+print('day 1, part 1: %d' % number_larger_than_previous(parsed_input))
+
+sliding_window_input = list(zip(parsed_input, parsed_input[1:], parsed_input[2:]))
+sliding_window_sums = [sum(i) for i in sliding_window_input]
+
+print('day 1, part 2: %d' % number_larger_than_previous(sliding_window_sums))
